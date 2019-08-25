3b56f3fc28bffe94f48870378f119f50d3f19f01
Gabriela <gsuarezc@unal.edu.co>
2019-08-24 21:34:18 -0500

    compare names changed

--- src/helpers/compare.js
+++ src/helpers/compare.js
@@ -1,14 +1,14 @@
-export function compare(a, b){
-  const price_a = a.subprice ? a.subprice : a.price;
-  const price_b = b.subprice ? b.subprice : b.price;
+export function compare(prod_a, prod_b){
+  const price_a = prod_a.subprice ? prod_a.subprice : prod_a.price;
+  const price_b = prod_b.subprice ? prod_b.subprice : prod_b.price;
 
   if (price_a > price_b) return 1;
   else return -1;
 }
 
-export function compareReverse(a, b){
-  const price_a = a.subprice ? a.subprice : a.price;
-  const price_b = b.subprice ? b.subprice : b.price;
+export function compareReverse(prod_a, prod_b){
+  const price_a = prod_a.subprice ? prod_a.subprice : prod_a.price;
+  const price_b = prod_b.subprice ? prod_b.subprice : prod_b.price;
 
   if (price_a > price_b) return -1;
   else return 1;
